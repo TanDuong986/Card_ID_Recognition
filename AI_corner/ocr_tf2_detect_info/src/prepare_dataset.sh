7ef3e9c6fb62fee0479ad7909adcdfcb13203c13
ChienVT <79775410+iamchien@users.noreply.github.com>
2023-05-19 15:01:53 +0700

    Delete AI_corner/ocr_tf2_detect_info directory

--- AI_corner/ocr_tf2_detect_info/src/prepare_dataset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python split_dataset.py
-python create_tf_record.py -x ../train_val_dataset/train/labels -l ./label_map.pbtxt -o ../train_val_dataset/train.record -i ../train_val_dataset/train/images
-python create_tf_record.py -x ../train_val_dataset/test/labels -l ./label_map.pbtxt -o ../train_val_dataset/test.record -i ../train_val_dataset/test/images
